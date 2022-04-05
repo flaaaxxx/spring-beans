e20a079eb3dae61860c34b04d3661b9b5ea70ec9
flaaaxxx <flaaaxxx@gmail.com>
2022-04-05 19:25:25 +0200

    Create Doctor bean in xml file

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -6,7 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
-//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Doctor doctor = context.getBean(Doctor.class);
+        doctor.assist();
     }
 
 }
