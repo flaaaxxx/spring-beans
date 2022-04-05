4923ed3616a0a07cf9f95ea3fce78c0f12087676
flaaaxxx <flaaaxxx@gmail.com>
2022-04-05 19:44:28 +0200

    New interface, fix bugs

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-        Doctor doctor = context.getBean(Doctor.class);
+        Staff doctor = context.getBean(Doctor.class);
         doctor.assist();
 
         Nurse nurse = (Nurse) context.getBean("nurse");
