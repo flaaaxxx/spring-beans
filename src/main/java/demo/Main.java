3a77541c6a59e261fc6c18c9b2486e0d6bf40a76
flaaaxxx <flaaaxxx@gmail.com>
2022-04-05 21:21:49 +0200

    AnnotationConfigurationContext

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,15 +1,20 @@
 package demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.lang.reflect.AnnotatedArrayType;
 
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-//        Doctor doctor = context.getBean(Doctor.class);
-//        doctor.assist();
+       ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+
+        Doctor doctor = context.getBean(Doctor.class);
+        doctor.assist();
 
 //        System.out.println(doctor.getQualification());
 
